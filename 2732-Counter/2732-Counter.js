206d2bb3cc000b5117c3bab1fd6bcf4363764d63
Ritanjit Das <ritanjitdas@gmail.com>
2026-05-27 20:31:13 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,17 @@
+// Last updated: 5/27/2026, 8:31:12 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n){
+    return function(){
+        return n++;
+    }
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
