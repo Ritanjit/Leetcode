ae4b93a3be41ec67269045ddce8d99c2e8c16e45
Ritanjit Das <ritanjitdas@gmail.com>
2026-05-27 23:29:14 +0530

    Create 2733-Sleep

---
+++ 2733-Sleep/2733-Sleep.js
@@ -0,0 +1,16 @@
+// Last updated: 5/27/2026, 11:29:14 PM
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    function callback(resolve, reject){
+        setTimeout(resolve, millis);
+    }
+    return new Promise(callback);
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
