18ea97c78a4acb379b3eb674963e9d578083134c
Ritanjit Das <ritanjitdas@gmail.com>
2026-05-27 23:29:18 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.js
@@ -0,0 +1,19 @@
+// Last updated: 5/27/2026, 11:29:18 PM
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    let count = 0;
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i]%2!==0) {
+            count++;
+            if (count===3) {
+                return true;
+            }
+        } else {
+            count=0;
+        }
+    }
+    return false;
+};
