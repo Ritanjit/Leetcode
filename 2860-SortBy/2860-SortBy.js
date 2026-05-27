cd2c96d3916af0d1c8e19195485e27d1be5dba5c
Ritanjit Das <ritanjitdas@gmail.com>
2026-05-27 23:28:31 +0530

    Create 2860-SortBy

---
+++ 2860-SortBy/2860-SortBy.js
@@ -0,0 +1,9 @@
+// Last updated: 5/27/2026, 11:28:31 PM
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a,b)=>fn(a)-fn(b))
+};
