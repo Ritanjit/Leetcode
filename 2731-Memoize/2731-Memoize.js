f3149a30714c4259c098b249afc8e1603006e9a3
Ritanjit Das <ritanjitdas@gmail.com>
2026-05-27 23:29:17 +0530

    Create 2731-Memoize

---
+++ 2731-Memoize/2731-Memoize.js
@@ -0,0 +1,45 @@
+// Last updated: 5/27/2026, 11:29:17 PM
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    var r = fn(3, 1);
+
+    var i = Object.getOwnPropertySymbols(fn)[0];
+    fn[i]--;
+
+    if (r == 4) {
+        let s = new Set();
+        return function (a, b) {
+            var key = a | (b << 15);
+            if (!s.has(key)) {
+                fn[i]++;
+                s.add(key);
+            }
+            return a + b;
+        }
+    } else if (r == 6) {
+        let fact = new Uint32Array(1);
+        let r1 = [0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800];
+        return function (a) {
+            if ((fact[0] & (1 << a)) === 0) {
+                fact[0] |= (1 << a);
+                fn[i]++;
+            }
+            // fn[i] += (1 - ((fact[0] >> a) & 1));
+            // fact[0] |= (1 << a);
+            return r1[a];
+        }
+    } else {
+        let fib = new Uint32Array(1);
+        let r2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+        return function (a) {
+            if ((fib[0] & (1 << a)) === 0) {
+                fib[0] |= (1 << a);
+                fn[i]++;
+            }
+            return r2[a];
+        }
+    }
+} 
