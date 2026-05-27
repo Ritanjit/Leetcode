142104f5009ae365f0fe8773dd167306a26802e8
Ritanjit Das <ritanjitdas@gmail.com>
2026-05-27 23:28:47 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 5/27/2026, 11:28:47 PM
+/**
+ * @return {Function}
+ */
+const createHelloWorld = function() {
+    
+    return function() {
+       return 'Hello World';
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
